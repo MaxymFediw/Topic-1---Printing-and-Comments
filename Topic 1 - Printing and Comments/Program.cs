48231dacf2a39c0b942d89cddf9ac9aa2f8cb43e
Maxym Fediw <362706764@GoTVDSB.ca>
2026-02-04 12:24:46 -0500

    Add project files.

---
+++ Topic 1 - Printing and Comments/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Topic_1___Printing_and_Comments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Maxym
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("My Name is Maxym Fediw");
+            Console.WriteLine();
+            Console.WriteLine("Comments are used for: 1. Improving readability, and 2. Maintainability.");
+            Console.WriteLine();
+            Console.WriteLine("M   M  FFFFF");
+            Console.WriteLine("MM MM  F");
+            Console.WriteLine("MM MM  F");
+            Console.WriteLine("M M M  FFF");
+            Console.WriteLine("M   M  F");
+            Console.WriteLine("M   M  F");
+            Console.WriteLine("M   M  F");
+
+
+
+
+
+
+
+
+        }
+    }
+}
